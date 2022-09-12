eb77ba5122d9898073c274557e90b8b96b51dc49
Daramfon <phonix.de@gmail.com>
2022-09-12 11:10:46 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description:size of data type
+ * this programme prints the size of the various date type
+ * Return: 0
+ * */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %ld  byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld  byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));        
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
