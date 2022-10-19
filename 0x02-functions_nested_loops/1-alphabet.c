90bbc323fda69d62f326bf91908fb55b4d111fea
Daramfon Etang <phonix.de@gmail.com>
2022-10-18 21:14:09 -0700

    nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabelt - prints the alpha in lowercase
+ */
+void print_alphabelt(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
