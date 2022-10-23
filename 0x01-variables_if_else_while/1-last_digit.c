5b6cbefa6c764cfae6e72d99680bcf851d01e24d
Daramfon Etang <phonix.de@gmail.com>
2022-10-23 04:46:20 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,16 +18,10 @@ int main(void)
 	l = n % 10;
 
 	if (l > 5)
-	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, 1);
-	}
-	else if (l == 0)
-	{
+	if (l == 0)
 		printf("Last digit of %d is %d and is 0\n", n, 1);
-	}
-	else
-	{
+	if (l < 6 && l != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-	}
 	return (0);
 }
