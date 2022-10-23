39ca1b2c8fe9b239253376c8650be5b83328cb1f
Daramfon Etang <phonix.de@gmail.com>
2022-10-23 14:39:29 -0700

    single digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: using the main function
+ * this project prints "all single digits of base 10
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+		while (i <= 9)
+		{
+			printf("%d", i);
+			i++;
+		}
+	printf("\n");
+	return (0);
+}
