22108a8f978d0d7d6646af5285a29494bb5dc4f6
Daramfon Etang <phonix.de@gmail.com>
2022-10-23 10:29:33 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Description: using the main function
+ * this a program to "print lower case alphabet
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	char Lc = 'a';
+
+	while (Lc <= 'z')
+	{
+		putchar(Lc);
+		Lc++;
+	}
+	return (0);
+}
+
