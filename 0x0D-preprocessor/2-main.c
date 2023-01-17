b7f76e660c3b60f6b31bfc28daa5f15edc1c6516
daetang <phonix.de@gmail.com>
2023-01-16 20:25:14 -0800

    file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print file name
+ * Description: a pogram that prints the name pf the file
+ * it was compiled from, followd by a new file
+ * Return: None
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
