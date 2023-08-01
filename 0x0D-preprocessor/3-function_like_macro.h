5ef266325b28ea747cbcfc6d5ba6c580aa3e121f
daetang <112046376+daetang@users.noreply.github.com>
2023-08-01 22:11:32 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef ABS_FUNCTION
-#define ABS_FUNCTION
-#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
-#endif /* ABS_FUNCTION */
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,4 +0,0 @@
-#ifndef SUM_FUNCTION
-#define SUM_FUNCTION
-#define SUM(x,y) ((x) +(y))
-#endif /* SUM_FUNCTION */
